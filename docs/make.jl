655fff6ea84161455646fe520957f5714d7503f1
Michael T. Enders <michael@michael-enders.com>
2025-01-13 15:44:33 +0100

    New documentation

--- docs/make.jl
+++ docs/make.jl
@@ -8,17 +8,25 @@ Pkg.instantiate()
 using Documenter
 using GeneralizedTransferMatrixMethod
 
+
+DocMeta.setdocmeta!(GeneralizedTransferMatrixMethod, :DocTestSetup, :(using
+    GeneralizedTransferMatrixMethod); recursive=true)
 makedocs(
     sitename="GeneralizedTransferMatrixMethod.jl",
     authors = "Michael T. Enders",
+    doctest = true,
+    modules = [GeneralizedTransferMatrixMethod],
     pages = [
-        "Introduction" => "index.md"
+        "Introduction" => "index.md",
         "Manual" => Any[
-            "gettingstarted.md",
-            "unitful.md",
-            # "parallelization.md",
-            # "examples.md"
-        ]
+             "gettingstarted.md",
+             "unitful.md"
+        ],
+        "Examples" => Any[
+            "sic-sphp.md",
+            "photonic-crystal.md",
+            "nonreciprocity.md"
+        ],
         "Library" => Any[
             "library-public.md",
             "library-internal.md"
